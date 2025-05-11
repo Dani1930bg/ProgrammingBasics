62498d4c9b1c64292208341be679497d4274bb97
Dani <daniel.panaiotov08@gmail.com>
2025-05-11 12:05:02 +0300

    Pet Shop task

---
+++ ProgrammingBasics/PetShop/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PetShop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int dogfoodcount = int.Parse(Console.ReadLine());
+        int catfoodcount = int.Parse(Console.ReadLine());
+        double dogfoodprice = dogfoodcount * 2.50;
+        int catfoodprice = catfoodcount * 4;
+        double totalprice = dogfoodprice + catfoodprice;
+        Console.WriteLine($"{totalprice} lv.");
+        
+    }
+}
