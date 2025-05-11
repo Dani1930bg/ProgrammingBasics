1f5b8602e285a0ac147ef84cc5fef171fe02cd7b
Dani <daniel.panaiotov08@gmail.com>
2025-05-11 11:32:45 +0300

    04. Inches to Centimeters  task

---
+++ ProgrammingBasics/InchestoCentimeters/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace InchestoCentimeters;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int inches = int.Parse(Console.ReadLine());
+        double sentimeters = inches * 2.54;
+        Console.WriteLine(sentimeters);
+        
+    }
+}
