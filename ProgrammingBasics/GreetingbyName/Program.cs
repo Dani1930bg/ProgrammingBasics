33da9d4169c8fcc462909dc8c7878291b526539e
Dani <daniel.panaiotov08@gmail.com>
2025-05-11 11:46:52 +0300

    Greeting by Name task

---
+++ ProgrammingBasics/GreetingbyName/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GreetingbyName;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       string name= Console.ReadLine();
+       Console.WriteLine($"Hello, {name}!");
+    }
+}
