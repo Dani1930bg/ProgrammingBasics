13712fcb06cc46554244e2ff50c06ece36f00aca
Dani <daniel.panaiotov08@gmail.com>
2025-05-11 11:57:46 +0300

    Projects Creation task

---
+++ ProgrammingBasics/ProjectsCreation/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectsCreation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        int projectscount = int.Parse(Console.ReadLine());
+        int projectstime = projectscount * 3;
+        Console.WriteLine($"The architect {name} will need {projectstime} hours to complete {projectscount} project/s.");
+    }
+}
