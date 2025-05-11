a5cb673fa61f2445d4bd36695806864b2aef5845
Dani <daniel.panaiotov08@gmail.com>
2025-05-11 12:12:14 +0300

    Yard Greening task

---
+++ ProgrammingBasics/YardGreening/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace YardGreening;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double plasetogreen = double.Parse(Console.ReadLine());
+        double pricefortheyard = plasetogreen * 7.61;
+        double discount = 0.18* pricefortheyard;
+        double totalprice = pricefortheyard - discount;
+        Console.WriteLine($"The final price is: {totalprice} lv.");
+        Console.WriteLine($"The discount is: {discount} lv.");
+    }
+}
