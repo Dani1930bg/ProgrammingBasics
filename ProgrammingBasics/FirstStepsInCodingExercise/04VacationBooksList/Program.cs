b60b31a5631d27ba44ddda3c7d594b52811cdf5d
Dani <daniel.panaiotov08@gmail.com>
2025-05-12 15:57:51 +0300

    04VacationBooksList task

---
+++ ProgrammingBasics/FirstStepsInCodingExercise/04VacationBooksList/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _04VacationBooksList;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int totalPages = int.Parse(Console.ReadLine());
+        int pagesPerHour = int.Parse(Console.ReadLine());
+        int days = int.Parse(Console.ReadLine());
+
+        int totalHours = totalPages / pagesPerHour;
+
+        int hoursPerDay = totalHours / days;
+
+        Console.WriteLine(hoursPerDay);
+    }
+}
