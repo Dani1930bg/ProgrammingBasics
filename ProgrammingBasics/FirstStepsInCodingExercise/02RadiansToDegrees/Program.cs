8296b05b50885f32db02db2205c5100d1c83003f
Dani <daniel.panaiotov08@gmail.com>
2025-05-12 14:59:58 +0300

    RadiansToDegrees task

---
+++ ProgrammingBasics/FirstStepsInCodingExercise/02RadiansToDegrees/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _02RadiansToDegrees;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double radians = double.Parse(Console.ReadLine());
+
+        double degrees = radians * 180 / Math.PI;
+
+        Console.WriteLine(degrees);
+    }
+}
