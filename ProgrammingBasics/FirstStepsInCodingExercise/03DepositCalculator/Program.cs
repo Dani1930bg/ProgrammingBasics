ac84d6daadc82d699fa6522594beea3ab60fd5c2
Dani <daniel.panaiotov08@gmail.com>
2025-05-12 15:54:49 +0300

    03DepositCalculator task

---
+++ ProgrammingBasics/FirstStepsInCodingExercise/03DepositCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _03DepositCalculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double depositedAmount = double.Parse(Console.ReadLine());
+        int months = int.Parse(Console.ReadLine());
+        double annualInterestRate = double.Parse(Console.ReadLine());
+
+        double interestForOneYear = depositedAmount * (annualInterestRate / 100);
+        double interestPerMonth = interestForOneYear / 12;
+
+        double totalAmount = depositedAmount + months * interestPerMonth;
+
+        Console.WriteLine(totalAmount);
+    }
+}
