446a0c85438da389179cec404a6cefe3126e0adb
Dani <daniel.panaiotov08@gmail.com>
2025-05-12 16:14:48 +0300

    09FishTank task

---
+++ ProgrammingBasics/FirstStepsInCodingExercise/09FishTank/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _09FishTank;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int length = int.Parse(Console.ReadLine());
+        int width = int.Parse(Console.ReadLine());
+        int height = int.Parse(Console.ReadLine());
+        double percent = double.Parse(Console.ReadLine());
+
+        double volumeCm3 = length * width * height;
+        double volumeLitres = volumeCm3 / 1000; 
+
+        double occupiedSpace = percent / 100;
+        double neededLitres = volumeLitres * (1 - occupiedSpace);
+
+        Console.WriteLine(neededLitres);
+    }
+}
