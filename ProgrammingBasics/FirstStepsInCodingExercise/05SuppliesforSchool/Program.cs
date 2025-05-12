fc6308f0ee97c715e1312d124bd6dbeafe301ee9
Dani <daniel.panaiotov08@gmail.com>
2025-05-12 16:00:27 +0300

    05SuppliesforSchool task

---
+++ ProgrammingBasics/FirstStepsInCodingExercise/05SuppliesforSchool/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _05SuppliesforSchool;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int penPacks = int.Parse(Console.ReadLine());
+        int markerPacks = int.Parse(Console.ReadLine());
+        int boardCleanerLiters = int.Parse(Console.ReadLine());
+        double discountPercent = double.Parse(Console.ReadLine()) / 100;
+
+        double totalPrice = (penPacks * 5.80) + (markerPacks * 7.20) + (boardCleanerLiters * 1.20);
+
+        double discountedPrice = totalPrice - (totalPrice * discountPercent);
+
+        Console.WriteLine(discountedPrice);
+    }
+}
