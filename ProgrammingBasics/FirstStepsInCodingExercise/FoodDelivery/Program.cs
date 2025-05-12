a0b43ba61e2116618a6571b4af430a1002cf7690
Dani <daniel.panaiotov08@gmail.com>
2025-05-12 16:04:53 +0300

    06FoodDelivery task

---
+++ ProgrammingBasics/FirstStepsInCodingExercise/FoodDelivery/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _06Repainting;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int chickenMenus = int.Parse(Console.ReadLine());
+        int fishMenus = int.Parse(Console.ReadLine());
+        int vegetarianMenus = int.Parse(Console.ReadLine());
+
+        double chickenMenuPrice = 10.35;
+        double fishMenuPrice = 12.40;
+        double vegetarianMenuPrice = 8.15;
+
+        double totalMenuPrice = (chickenMenus * chickenMenuPrice) + (fishMenus * fishMenuPrice) + (vegetarianMenus * vegetarianMenuPrice);
+
+        double dessertPrice = totalMenuPrice * 0.20;
+
+        double deliveryFee = 2.50;
+
+        double totalPrice = totalMenuPrice + dessertPrice + deliveryFee;
+
+        Console.WriteLine(totalPrice);
+        
+    }
+}
