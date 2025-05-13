dbb0a913c0f6306d3cbb4102e9be66ac9b5ce434
Dani <daniel.panaiotov08@gmail.com>
2025-05-13 17:45:32 +0300

    Number100to200 task

---
+++ ProgrammingBasics/ConditionalStatementsLab/Number100to200/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Design;
+
+namespace Number100to200;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       int number1 = int.Parse(Console.ReadLine());
+       int number2 = 100;
+       int number3 = 200;
+
+       if (number1 >= number2 && number1 <= number3)
+       {
+           Console.WriteLine("Between 100 and 200");
+       }
+      else if (number1 < number2)
+      {
+          Console.WriteLine("Less than 100");
+      }
+       else if (number1 > number3)
+       {
+           Console.WriteLine("Greater than 200");
+       }
+       
+       
+    }
+}
