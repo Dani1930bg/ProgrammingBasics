ec0c65ea739a9cbe2855e137d6c52a797c039e79
Dani <daniel.panaiotov08@gmail.com>
2025-05-13 17:51:41 +0300

    07AreaofFigures task

---
+++ ProgrammingBasics/ConditionalStatementsLab/07AreaofFigures/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _07AreaofFigures;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string figure = Console.ReadLine();
+
+        if (figure == "square")
+        {
+            double side = double.Parse(Console.ReadLine());
+            double area = side * side;
+            Console.WriteLine($"{area:F3}");
+        }
+        else if (figure == "rectangle")
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double area = length * width;
+            Console.WriteLine($"{area:F3}");
+        }
+        else if (figure == "circle")
+        {
+            double radius = double.Parse(Console.ReadLine());
+            double area = Math.PI * radius * radius;
+            Console.WriteLine($"{area:F3}");
+        }
+        else if (figure == "triangle")
+        {
+            double baseLength = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = (baseLength * height) / 2;
+            Console.WriteLine($"{area:F3}");
+        }
+    }
+}
