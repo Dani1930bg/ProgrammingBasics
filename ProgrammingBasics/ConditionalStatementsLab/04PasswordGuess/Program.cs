360613c4f76c6cae93415d506b9fcc3541b0d046
Dani <daniel.panaiotov08@gmail.com>
2025-05-13 17:36:54 +0300

    04PasswordGuess task

---
+++ ProgrammingBasics/ConditionalStatementsLab/04PasswordGuess/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04PasswordGuess;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       string triedpassword = Console.ReadLine();
+       string password = "s3cr3t!P@ssw0rd";
+       if (triedpassword == password)
+       {
+           Console.WriteLine("Welcome");
+       }
+       else
+       {
+           Console.WriteLine("Wrong password!");
+       }
+    } 
+}
