cd7ad50562cfdb32a2b661bbad64caf6e5de06e0
Dani <daniel.panaiotov08@gmail.com>
2025-05-14 22:37:52 +0300

    06WorldSwimmingRecord task

---
+++ ProgrammingBasics/ConditionalStatementsExercise/06WorldSwimmingRecord/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _06WorldSwimmingRecord;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double recordInSeconds = double.Parse(Console.ReadLine());
+        double distanceInMeters = double.Parse(Console.ReadLine());
+        double timePerMeter = double.Parse(Console.ReadLine());
+
+        double swimTime = distanceInMeters * timePerMeter;
+
+        double delay = Math.Floor(distanceInMeters / 15) * 12.5;
+
+        double totalTime = swimTime + delay;
+
+        if (totalTime < recordInSeconds)
+        {
+            Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:F2} seconds.");
+        }
+        else
+        {
+            double difference = totalTime - recordInSeconds;
+            Console.WriteLine($"No, he failed! He was {difference:F2} seconds slower.");
+        }
+    }
+}
