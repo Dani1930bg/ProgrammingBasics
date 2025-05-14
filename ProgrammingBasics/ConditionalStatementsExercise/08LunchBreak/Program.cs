7323188205de2a093ffc62980f1a1333183f8c85
Dani <daniel.panaiotov08@gmail.com>
2025-05-14 22:40:29 +0300

    08LunchBreak task

---
+++ ProgrammingBasics/ConditionalStatementsExercise/08LunchBreak/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _08LunchBreak;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string seriesName = Console.ReadLine();
+        int episodeDuration = int.Parse(Console.ReadLine());
+        int breakDuration = int.Parse(Console.ReadLine());
+
+        double lunchTime = breakDuration / 8.0;
+        double relaxTime = breakDuration / 4.0;
+        double timeLeft = breakDuration - lunchTime - relaxTime;
+
+        if (timeLeft >= episodeDuration)
+        {
+            double freeTime = Math.Ceiling(timeLeft - episodeDuration);
+            Console.WriteLine($"You have enough time to watch {seriesName} and left with {freeTime} minutes free time.");
+        }
+        else
+        {
+            double neededTime = Math.Ceiling(episodeDuration - timeLeft);
+            Console.WriteLine($"You don't have enough time to watch {seriesName}, you need {neededTime} more minutes.");
+        }
+    }
+}
