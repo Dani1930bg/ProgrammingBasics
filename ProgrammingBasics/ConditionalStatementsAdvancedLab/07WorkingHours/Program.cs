bdf965ddcbef95d602555c845120def9dee8632f
Dani <daniel.panaiotov08@gmail.com>
2025-05-15 16:11:58 +0300

    07WorkingHours task

---
+++ ProgrammingBasics/ConditionalStatementsAdvancedLab/07WorkingHours/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _07WorkingHours;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int hour = int.Parse(Console.ReadLine());
+        string day = Console.ReadLine();
+
+        bool isWorkingDay = day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday" || day == "Saturday";
+
+        if (hour >= 10 && hour <= 18 && isWorkingDay)
+        {
+            Console.WriteLine("open");
+        }
+        else
+        {
+            Console.WriteLine("closed");
+        }
+    }
+}
