744fc7de991470bdb0ec4cc8f0e23389df809429
Dani <daniel.panaiotov08@gmail.com>
2025-05-15 16:15:04 +0300

    08CinemaTicket task

---
+++ ProgrammingBasics/ConditionalStatementsAdvancedLab/08CinemaTicket/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _08CinemaTicket;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string day = Console.ReadLine();
+
+        switch (day)
+        {
+            case "Monday":
+                case "Tuesday":
+                Console.WriteLine(12);
+                break;
+            case "Wednesday":
+                case "Thursday":
+                Console.WriteLine(14);
+                break;
+            case "Friday":
+                Console.WriteLine(12);
+                break;
+            case "Saturday":
+                case "Sunday":
+                Console.WriteLine(16);
+                break;
+        }
+        
+    }
+}
