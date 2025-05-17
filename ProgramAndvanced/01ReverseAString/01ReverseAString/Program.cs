d52f606babb01c61976d7bfe91a0143ee2b51f22
Dani <daniel.panaiotov08@gmail.com>
2025-05-17 11:07:44 +0300

    01ReverseAString task

---
+++ ProgramAndvanced/01ReverseAString/01ReverseAString/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01ReverseAString;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputTetxToReverse = Console.ReadLine();
+        Stack<char> reversingStack = new Stack<char>();
+        
+        foreach (char symbol in inputTetxToReverse)
+        {
+            reversingStack.Push(symbol);
+        }
+
+        while (reversingStack.Count > 0)
+        {
+            Console.Write(reversingStack.Pop());
+        }
+    }
+}
