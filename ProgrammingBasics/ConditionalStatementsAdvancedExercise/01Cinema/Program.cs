ec27e68263438eba6fdb4c4b97d8e5c544835472
Dani <daniel.panaiotov08@gmail.com>
2025-06-01 13:10:53 +0300

    01Cinema task

---
+++ ProgrammingBasics/ConditionalStatementsAdvancedExercise/01Cinema/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01Cinema;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string projectionType = Console.ReadLine();
+        int rows = int.Parse(Console.ReadLine());
+        int columns = int.Parse(Console.ReadLine());
+
+        int totalSeats = rows * columns;
+        double pricePerTicket = 0.0;
+
+        if (projectionType == "Premiere")
+        {
+            pricePerTicket = 12.00;
+        }
+        else if (projectionType == "Normal")
+        {
+            pricePerTicket = 7.50;
+        }
+        else if (projectionType == "Discount")
+        {
+            pricePerTicket = 5.00;
+        }
+
+        double totalIncome = totalSeats * pricePerTicket;
+        Console.WriteLine("{0:f2} leva", totalIncome);
+    }
+}
